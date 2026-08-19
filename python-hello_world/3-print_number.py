91eb8d76abb0ae937816bde13d691207eada547a
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-08-19 06:34:14 -0700

    task 003

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number1=333 , number2=-98 , number3=0
-print(f"{number1} Battery street") print(f"{number2} Battery street") print(f"{number3} Battery street")
+number=98
+print(f"{number} Battery street")
