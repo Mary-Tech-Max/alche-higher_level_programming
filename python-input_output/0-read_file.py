cc2dc4d27f6f4f475b7c55213f6e65b07d1572a4
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-09-09 09:45:37 -0700

    task 0 complete

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
-```python
 #!/usr/bin/python3
+"""Module for reading a UTF-8 text file."""
 
 
 def read_file(filename=""):
     """Read a UTF-8 text file and print its contents to stdout."""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
-```
