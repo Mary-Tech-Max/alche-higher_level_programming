b7ee2910c09e4e35f488da54a9cde0fd79d27254
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-09-13 08:40:43 -0700

    correcting task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- 16. Shows by genre
 
-SELECT tv_genres.name, tv_shows.title
-FROM tv_genres
-JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_genres.name ASC, tv_shows.title ASC;
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
