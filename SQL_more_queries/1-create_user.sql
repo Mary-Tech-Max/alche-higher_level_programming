0387c0b715d33eff031232c682effe4e3ef6b955
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-09-13 08:00:51 -0700

    more sqls

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,10 @@
--- 0. My privileges!
+-- 1. Root user
 
--- Show privileges of user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Create user_0d_1 if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
--- Show privileges of user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Set the password for user_0d_1
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
