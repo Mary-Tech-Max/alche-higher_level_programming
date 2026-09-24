37c4da17d5a0b27a30f8a7a5b8f43deffc0c857d
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-09-24 18:51:21 +0000

    added a readme file and modifies file 3

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Sends a POST request with an email to a URL and displays the body."""
+"""Displays the body of a URL response or the HTTP error code."""
 import sys
-import urllib.parse
+import urllib.error
 import urllib.request
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("utf-8")
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
