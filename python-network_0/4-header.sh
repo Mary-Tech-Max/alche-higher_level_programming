1ee2812d37ed71682d8d0bfd0fdcd129dcc5c62a
Mary-Tech-Max <m.njuguna@alustudent.com>
2026-09-24 16:33:09 +0000

    modified file 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header
+# Sends a GET request to a URL with the header X-HolbertonSchool-User-Id set to 98 and displays the body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
